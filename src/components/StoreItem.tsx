bb8394980e0bafcb14048fa5fe0f2198b20f9a75
Richard-L <rrlaw93@gmail.com>
2024-03-13 15:12:02 +0000

    Add to cart + buttons

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,3 +1,7 @@
+import { formarCurrency } from "../utilities/currencyformat"
+import "../style.css"
+
+
 type StoreItemProps = {
     id: number
     name: string
@@ -8,14 +12,43 @@ type StoreItemProps = {
 
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+
+    const quantity = 0
     return <>
     <div>
-        <img src={imgUrl} style={{ height: "100%", width: "80%", margin:"2%"}}></img>
+        <img src={imgUrl} style={{ height: "auto", width: "80%", margin:"2%"}}></img>
         <div className="d-flex justify-content-between align-items-baseline mb-4">
             <span className="">{name}</span>
-            <span>{price}</span>
+            <span>{formarCurrency (price)}</span>
 
-        </div>
+            </div>
+
+            <div className="mt-auto">
+                {quantity === 0 ? (
+
+                    <button className="w-100"> + Add To Cart </button>
+
+                ) : <div> 
+                        <div className="addBtn" style={{gap: ".5rem"}} >
+                            
+                            <button>-</button>
+                            <span className="fs-3"> {quantity + " in cart"} </span>
+                            <button>+</button>
+
+                        </div>
+
+
+                        <div className="rmvBtn">
+                            
+                            <button className="bg-danger"> Remove </button>
+                
+                        </div> 
+                
+                    </div>}
+            </div>
+
+
+       
     </div>
     </>
 }
--- src/pages/store.tsx
+++ src/pages/store.tsx
@@ -5,7 +5,10 @@ import { StoreItem } from "../components/StoreItem"
 export function Store() {
     return <>
     <h1>Store</h1>
+    <div className="cardp">
 
         {storeItems.map(item => ( <div className="card" key={item.id}><StoreItem {...item}/></div>))}
+    </div>
+   
     </>
 };
